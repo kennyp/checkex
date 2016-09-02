b94adf93e20abd5d9a30bb6bb2cb5b7f08d43991
Kenny Parnell <k.parnell@gmail.com>
2016-09-02 04:39:27 -0400

    Get basic parsing working.

--- lib/checkex.ex
+++ lib/checkex.ex
@@ -1,2 +1,84 @@
 defmodule Checkex do
+  defstruct title: "", steps: []
+
+  defmodule Step, do: defstruct item: "", body: []
+  defmodule Text, do: defstruct content: ""
+  defmodule Command, do: defstruct content: ""
+  defmodule CommandBlock, do: defstruct language: "", commands: []
+
+  def parse(source) when is_binary(source) do
+    source
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> parse_lines
+  end
+
+  defp parse_lines(["#" <> title | body]) do
+    steps = parse_body(body)
+    struct(__MODULE__, title: String.trim(title), steps: steps)
+  end
+
+  defp parse_body([]), do: []
+
+  defp parse_body(body) do
+    parse_body([], body)
+  end
+
+  defp parse_body(list, ["-" <> line | rest]) do
+    step = struct(Step, item: String.trim(line))
+    parse_children(list, step, rest)
+  end
+
+  defp parse_children(list, step, body = ["-" <> _line | _rest]) do
+    step = struct(step, body: Enum.reverse(step.body))
+    parse_body([step | list], body)
+  end
+
+  defp parse_children(list, step, [line | rest]) do
+    {step, rest} = update_step(step, parse_line(line), rest)
+    parse_children(list, step, rest)
+  end
+
+  defp parse_children(list, step, []) do
+    [step | list] |> Enum.reverse
+  end
+
+  defp update_step(step, block=%CommandBlock{}, body = ["-" <> _line | _rest]) do
+    step = struct(step, body: block.commands ++ step.body)
+    {step, body}
+  end
+
+  defp update_step(step, block=%CommandBlock{commands: cs}, [line | rest]) do
+    case parse_line(line) do
+      %CommandBlock{} ->
+        update_step(step, block, rest)
+      %Text{content: line} ->
+        command = struct(Command, content: line)
+        update_step(step, struct(block, commands: [command | cs]), rest)
+    end
+  end
+
+  defp update_step(step, line, rest) do
+    step = struct(step, body: [line | step.body])
+    {step, rest}
+  end
+
+  defp parse_line("```" <> language) do
+    struct(CommandBlock, language: language)
+  end
+
+  defp parse_line("`" <> line) do
+    command = String.trim_trailing(line, "`")
+    struct(Command, content: command)
+  end
+
+  defp parse_line(line) do
+    struct(Text, content: line)
+  end
+
+  defimpl Enumerable do
+    def count(list), do: Enumerable.List.count(list.steps)
+    def member?(list, element), do: Enumerable.List.member?(list.steps, element)
+    def reduce(list, acc, fun), do: Enumerable.List.reduce(list.steps, acc, fun)
+  end
 end
--- test/checkex_test.exs
+++ test/checkex_test.exs
@@ -1,8 +1,153 @@
 defmodule CheckexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Checkex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Checkex.{Text,Command}
+
+
+  describe "when working with a simple checklist" do
+    setup [:parse_simple_list]
+
+    test "that we can fetch the title", %{rendered_list: rendered_list} do
+      assert "ToDo" == rendered_list.title
+    end
+
+    test "that we can get at the steps", %{rendered_list: rendered_list} do
+      assert 3 == Enum.count(rendered_list.steps)
+
+      items = Enum.map(rendered_list.steps, &(&1.item))
+      assert ["Step One", "Step Two", "Step Three"] == items
+
+      expected_bodies = [
+        [%Text{content: "Do some stuff!"}],
+        [%Text{content: "Do some more stuff!"}],
+        [%Text{content: "Do the last stuff!"}]
+      ]
+      actual_bodies = Enum.map(rendered_list.steps, &(&1.body))
+      assert expected_bodies == actual_bodies
+    end
+  end
+
+  describe "when working with a checklist containing shell commands" do
+    setup [:parse_command_list]
+
+    test "that we can fetch the title", %{rendered_list: rendered_list} do
+      assert "Commands ToDo" == rendered_list.title
+    end
+
+    test "that we can get at the steps", %{rendered_list: rendered_list} do
+      assert 2 == Enum.count(rendered_list.steps)
+
+      items = Enum.map(rendered_list.steps, &(&1.item))
+      assert ["Step One", "Step Two"] == items
+
+      expected_bodies = [
+        [%Text{content: "Do some stuff!"}],
+        [%Command{content: "echo \"Hello World\""}],
+      ]
+      actual_bodies = Enum.map(rendered_list.steps, &(&1.body))
+      assert expected_bodies == actual_bodies
+    end
+  end
+
+  describe "when working with a checklist whose items have multiple body lines" do
+    setup [:parse_complex_list]
+
+    test "that we can fetch the title", %{rendered_list: rendered_list} do
+      assert "Complex ToDo" == rendered_list.title
+    end
+
+    test "that we can get at the steps", %{rendered_list: rendered_list} do
+      assert 4 == Enum.count(rendered_list.steps)
+
+      items = Enum.map(rendered_list.steps, &(&1.item))
+      assert ["Step One", "Step Two", "Step Three", "Step Four"] == items
+
+      expected_bodies = [
+        [%Text{content: "Do some stuff!"}],
+        [
+          %Text{content: "Make sure we're ready to rock!"},
+          %Command{content: "echo \"Hello World\""}
+        ],
+        [
+          %Text{content: "Here's the big one!"},
+          %Checkex.Command{content: "./run_my_script"},
+          %Checkex.Command{content: "echo \"Goodbye World!\""} 
+        ],
+        [%Text{content: "Finish it off"}],
+      ]
+      actual_bodies = Enum.map(rendered_list.steps, &(&1.body))
+      assert expected_bodies == actual_bodies
+    end
+  end
+
+  defp parse_simple_list(context) do
+    simple_list = """
+      # ToDo
+
+      - Step One
+
+        Do some stuff!
+
+      - Step Two
+
+        Do some more stuff!
+
+      - Step Three
+
+        Do the last stuff!
+
+      """
+    rendered_list = Checkex.parse(simple_list)
+    Map.put(context, :rendered_list, rendered_list)
+  end
+
+  defp parse_command_list(context) do
+    command_list = """
+      # Commands ToDo
+
+      - Step One
+
+        Do some stuff!
+
+      - Step Two
+
+        `echo "Hello World"`
+
+      """
+    rendered_list = Checkex.parse(command_list)
+    Map.put(context, :rendered_list, rendered_list)
+  end
+
+  defp parse_complex_list(context) do
+    complex_list = """
+      # Complex ToDo
+
+      - Step One
+
+        Do some stuff!
+
+      - Step Two
+
+        Make sure we're ready to rock!
+
+        `echo "Hello World"`
+
+      - Step Three
+
+        Here's the big one!
+
+        ```bash
+        ./run_my_script
+        echo "Goodbye World!"
+        ```
+
+      - Step Four
+
+        Finish it off
+
+      """
+    rendered_list = Checkex.parse(complex_list)
+    Map.put(context, :rendered_list, rendered_list)
   end
 end
